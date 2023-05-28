71243a519f64cdb6895e4a49c9a5b4552973d202
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-05-28 17:25:30 +0900

    CreateFormとEditFormの切り替え処理を修正
    ・letを使用して変数を宣言してからif文の返り値を代入していたが、関数の返り値を直接代入する形に変更した。

--- src/FormSwitcher.jsx
+++ src/FormSwitcher.jsx
@@ -5,29 +5,30 @@ import EditForm from './EditForm.jsx'
 
 export default class FormSwitcher extends React.Component {
   render () {
-    let form
-    if (this.props.isChanging) {
-      form = (
-        <EditForm
-          editingMemoText={this.props.editingMemoText}
-          onEditingMemoTextChange={this.props.onEditingMemoTextChange}
-          updateMemo={this.props.updateMemo}
-          closeEditForm={this.props.closeEditForm}
-          selectedMemo={this.props.selectedMemo}
-          indexOfSelectedMemo={this.props.indexOfSelectedMemo}
-          deleteMemo={this.props.deleteMemo}
-        />
-      )
-    } else if (this.props.isCreating) {
-      form = (
-        <CreateForm
-          newMemoText={this.props.newMemoText}
-          onNewMemoTextChange={this.props.onNewMemoTextChange}
-          addMemo={this.props.addMemo}
-          closeCreateForm={this.props.closeCreateForm}
-        />
-      )
-    }
+    const form = ((isChanging, isCreating) => {
+      if (isChanging) {
+        return (
+          <EditForm
+            editingMemoText={this.props.editingMemoText}
+            onEditingMemoTextChange={this.props.onEditingMemoTextChange}
+            updateMemo={this.props.updateMemo}
+            closeEditForm={this.props.closeEditForm}
+            selectedMemo={this.props.selectedMemo}
+            indexOfSelectedMemo={this.props.indexOfSelectedMemo}
+            deleteMemo={this.props.deleteMemo}
+          />
+        )
+      } else if (isCreating) {
+        return (
+          <CreateForm
+            newMemoText={this.props.newMemoText}
+            onNewMemoTextChange={this.props.onNewMemoTextChange}
+            addMemo={this.props.addMemo}
+            closeCreateForm={this.props.closeCreateForm}
+          />
+        )
+      }
+    })(this.props.isChanging, this.props.isCreating)
     return form
   }
 }
