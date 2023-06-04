2443ba00ff00ec8bb7b0dc33bc7b15b513cfba3a
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-06-04 13:51:49 +0900

    フォームの表示切替時に三項演算子の中でdivタグが重複していた部分を外に出した

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,42 +6,42 @@ export default class Form extends React.Component {
   render () {
     return (
       <div>
-        {this.props.indexOfSelectedMemo ? <p>編集</p> : <p>新規登録</p>}
+        <p>{this.props.indexOfSelectedMemo ? '編集' : '新規登録'}</p>
         <textarea
           value={this.props.newMemoText}
           onChange={this.props.handleNewMemoTextChange}
         />
-        {this.props.indexOfSelectedMemo
-          ? (
-          <div>
-            <button
-              type="submit"
-              onClick={(event) =>
-                this.props.updateMemo(this.props.indexOfSelectedMemo, event)
-              }
-            >
-              更新
-            </button>
-            <button
-              type="button"
-              onClick={(event) =>
-                this.props.deleteMemo(this.props.indexOfSelectedMemo, event)
-              }
-            >
-              削除
-            </button>
-          </div>
-            )
-          : (
-          <div>
+        <div>
+          {this.props.indexOfSelectedMemo
+            ? (
+            <div>
+              <button
+                type="submit"
+                onClick={(event) =>
+                  this.props.updateMemo(this.props.indexOfSelectedMemo, event)
+                }
+              >
+                更新
+              </button>
+              <button
+                type="button"
+                onClick={(event) =>
+                  this.props.deleteMemo(this.props.indexOfSelectedMemo, event)
+                }
+              >
+                削除
+              </button>
+            </div>
+              )
+            : (
             <button
               type="submit"
               onClick={(event) => this.props.addMemo(event)}
             >
               登録
             </button>
-          </div>
-            )}
+              )}
+        </div>
         <button type="button" onClick={this.props.closeForm}>
           キャンセル
         </button>
