e9cdf9129097caa0ac4dbf85b7cd3beda77db564
ham-cap <66000707+ham-cap@users.noreply.github.com>
2023-08-06 12:31:30 +0900

    Revert "React Hooksの課題"

--- src/MemosSection.jsx
+++ src/MemosSection.jsx
@@ -1,41 +1,65 @@
-import { React, useState } from 'react'
+import React from 'react'
 import Form from './Form.jsx'
 import MemoList from './MemoList.jsx'
-import { useMemos } from './useMemos.js'
 import './style/MemoList.css'
 
-function MemosSection () {
-  const [isVisibleForm, setIsVisibleForm] = useState(false)
-  const [indexOfSelectedMemo, setIndexOfSelectedMemo] = useState(null)
-  const memos = useMemos()
-
-  const closeForm = () => {
-    setIsVisibleForm(false)
-    setIndexOfSelectedMemo('')
+export default class MemosSection extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      memos: [],
+      isVisible: false,
+      indexOfSelectedMemo: null
+    }
+    this.displayForm = this.displayForm.bind(this)
+    this.closeForm = this.closeForm.bind(this)
   }
 
-  const displayForm = (e, index = null) => {
+  componentDidMount () {
+    const currentMemos = this.getCurrentMemos()
+    this.setState({ memos: currentMemos })
+  }
+
+  getCurrentMemos () {
+    const memosJson = localStorage.getItem('memos')
+    if (memosJson === null) {
+      return []
+    } else {
+      const memosJson = localStorage.getItem('memos')
+      const memos = JSON.parse(memosJson)
+      return memos
+    }
+  }
+
+  closeForm () {
+    this.setState({ isVisible: false, selectedMemo: '' })
+  }
+
+  displayForm (e, index = null) {
     e.preventDefault()
-    setIsVisibleForm(true)
-    setIndexOfSelectedMemo(index)
+    this.setState({
+      isVisible: true,
+      indexOfSelectedMemo: index
+    })
   }
 
-  return (
-    <div className="listContainer">
-      <div>
-        <MemoList memos={memos} displayForm={displayForm} />
-        <button className="linkForCreateForm" onClick={displayForm}>
-          +
-        </button>
+  render () {
+    return (
+      <div className="listContainer">
+        <div>
+          <MemoList memos={this.state.memos} displayForm={this.displayForm} />
+          <button className="linkForCreateForm" onClick={this.displayForm}>
+            +
+          </button>
+        </div>
+        {this.state.isVisible && (
+          <Form
+            memos={this.state.memos}
+            indexOfSelectedMemo={this.state.indexOfSelectedMemo}
+            closeForm={this.closeForm}
+          />
+        )}
       </div>
-      {isVisibleForm && (
-        <Form
-          memos={memos}
-          indexOfSelectedMemo={indexOfSelectedMemo}
-          closeForm={closeForm}
-        />
-      )}
-    </div>
-  )
+    )
+  }
 }
-export default MemosSection
